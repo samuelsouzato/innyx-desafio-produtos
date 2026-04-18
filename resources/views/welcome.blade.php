b819b81d515146a59a325724bd8fad17b38e282e
Sam <samuel.souzato@gmail.com>
2026-04-18 15:32:35 -0400

    teste

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Innyx Desafio</title>
-     <script type="module" crossorigin src="/assets/index-D9JRmpiq.js"></script>
+    <script type="module" crossorigin src="/assets/index-D9JRmpiq.js"></script>
     <link rel="modulepreload" crossorigin href="/assets/usetoast-CD6pPcYs.js">
     <link rel="modulepreload" crossorigin href="/assets/useconfirm-DFC6DQLe.js">
     <link rel="stylesheet" crossorigin href="/assets/index-D8jnkdJn.css">
